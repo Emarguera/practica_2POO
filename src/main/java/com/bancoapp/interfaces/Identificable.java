6ad7646de4f8119eca32152886a85f41d50ae83a
Esteban Ruiz <eruizm@ucenfotec.ac.cr>
2025-06-25 20:13:08 -0600

    Avance segunda practica en clase POO // Integración de interfaces

---
+++ src/main/java/com/bancoapp/interfaces/Identificable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.bancoapp.interfaces;
+
+/**
+ *
+ * @author Emarguera
+ */
+public interface Identificable {
+    String obtenerIdentificador();
+}
---
+++ src/main/java/com/bancoapp/interfaces/Registrable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.bancoapp.interfaces;
+
+/**
+ *
+ * @author Emarguera
+ */
+public interface Registrable {
+    void registrar();
+    void eliminar();
+    void listar();
+}
